5e9819f56e57ca90617e5a745bd36a19f445b6c1
animeboi0202-dotcom <animeboi0202@gmail.com>
2025-10-31 17:24:25 +0000

    Your descriptive commit message here

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import { createToolbar } from './components/Toolbar.js';
+
+// Import the video player component
+import { createVideoPlayer } from './components/VideoPlayer.js';
+
+// Run the functions to build the page
+createToolbar();
+createVideoPlayer();
